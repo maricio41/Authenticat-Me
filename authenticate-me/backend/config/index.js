6584bd4f12382388aca8074ba07317718345c09f
Maricio Harris <maricioharris@gmail.com>
2021-03-25 16:14:07 -0400

    Phase 0 completed

--- authenticate-me/backend/config/index.js
+++ authenticate-me/backend/config/index.js
@@ -4,7 +4,7 @@ module.exports = {
   db: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DATABASE,
+    database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
   },
   jwtConfig: {
