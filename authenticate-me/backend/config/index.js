b0b7473c7809c2f1dd440f0e154d00ca9ee51c5d
Maricio Harris <maricioharris@gmail.com>
2021-03-25 13:04:08 -0400

    Moving along'

--- authenticate-me/backend/config/index.js
+++ authenticate-me/backend/config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  environment: ProcessingInstruction.env.NODE_ENV || "development",
-  port: ProcessingInstruction.env.PORT || 5000,
+  environment: process.env.NODE_ENV || "development",
+  port: process.env.PORT || 5000,
   db: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
