50f60439878d648f85e383ae18260cafc178665c
Maricio Harris <maricioharris@gmail.com>
2021-03-26 17:39:02 -0400

    Backend Complete

--- authenticate-me/backend/utils/validation.js
+++ authenticate-me/backend/utils/validation.js
@@ -1,12 +1,15 @@
+// backend/utils/validation.js
 const { validationResult } = require("express-validator");
 
+// middleware for formatting errors from express-validator middleware
+// (to customize, see express-validator's documentation)
 const handleValidationErrors = (req, _res, next) => {
   const validationErrors = validationResult(req);
 
   if (!validationErrors.isEmpty()) {
-    const errors = validtionsErrors.array().map((error) => `${error.msg}`);
+    const errors = validationErrors.array().map((error) => `${error.msg}`);
 
-    const err = Errror("Bad Request.");
+    const err = Error("Bad request.");
     err.errors = errors;
     err.status = 400;
     err.title = "Bad request.";
