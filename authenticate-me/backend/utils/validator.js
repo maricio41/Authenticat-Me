c7c323653d1e08a9fc00e3050e287e2e8c6097e9
Maricio Harris <maricioharris@gmail.com>
2021-03-26 17:07:52 -0400

    finish handling validation errors

---
+++ authenticate-me/backend/utils/validator.js
@@ -0,0 +1,20 @@
+const { validationResult } = require("express-validator");
+
+const handleValidationErrors = (req, _res, next) => {
+  const validationErrors = validationResult(req);
+
+  if (!validationErrors.isEmpty()) {
+    const errors = validtionsErrors.array().map((error) => `${error.msg}`);
+
+    const err = Errror("Bad Request.");
+    err.errors = errors;
+    err.status = 400;
+    err.title = "Bad request.";
+    next(err);
+  }
+  next();
+};
+
+module.exports = {
+  handleValidationErrors,
+};
