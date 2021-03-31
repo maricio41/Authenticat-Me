76bb8b2f0ea5f89cfcea4a3bcea456c0a9e34b16
Maricio Harris <maricioharris@gmail.com>
2021-03-30 21:45:09 -0400

    created and migrated Businesses Table

---
+++ backend/db/migrations/20210331013923-create-business.js
@@ -0,0 +1,70 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("Businesses", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING(100),
+      },
+      address1: {
+        allowNull: false,
+        type: Sequelize.STRING(200),
+      },
+      address2: {
+        type: Sequelize.STRING(200),
+      },
+      city: {
+        allowNull: false,
+        type: Sequelize.STRING(30),
+      },
+      state: {
+        allowNull: false,
+        type: Sequelize.STRING(30),
+      },
+      postCode: {
+        allowNull: false,
+        type: Sequelize.STRING(10),
+      },
+      phoneNumber: {
+        allowNull: false,
+        type: Sequelize.STRING(12),
+        unique: true,
+      },
+      hours: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      bizUrl: {
+        allowNull: false,
+        type: Sequelize.STRING(255),
+        unique: true,
+      },
+      yearsInBusiness: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      ownerId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        unique: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("Businesses");
+  },
+};
---
+++ backend/db/models/business.js
@@ -0,0 +1,20 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Business = sequelize.define('Business', {
+    name: DataTypes.STRING,
+    address1: DataTypes.STRING,
+    address2: DataTypes.STRING,
+    city: DataTypes.STRING,
+    state: DataTypes.STRING,
+    postCode: DataTypes.STRING,
+    phoneNumber: DataTypes.STRING,
+    hours: DataTypes.TEXT,
+    bizUrl: DataTypes.STRING,
+    yearsInBusiness: DataTypes.INTEGER,
+    ownerId: DataTypes.INTEGER
+  }, {});
+  Business.associate = function(models) {
+    // associations can be defined here
+  };
+  return Business;
+};
