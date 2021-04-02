0765b5ce6530e56fd8df51ca825ab61a5de95b45
Maricio Harris <maricioharris@gmail.com>
2021-04-02 13:52:24 -0400

    added css to reviews component

--- frontend/src/components/ReviewList/index.js
+++ frontend/src/components/ReviewList/index.js
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getReviews } from "../../store/reviews";
+import "./ReviewList.css";
 
 function ReviewList() {
   const reviews = useSelector((state) => state.reviews);
@@ -13,9 +14,16 @@ function ReviewList() {
   const renderReviews = () => {
     return Object.values(reviews).map((review) => {
       return (
-        <li className="review-panel">
-          {review.content} Rating: {review.rating} By: {review.User.userName}
-        </li>
+        <div className="review-panel">
+          <div className="inner-review-components">Business Name Goes here</div>
+          <div className="inner-review-components">{`"${review.content}"`}</div>
+          <div className="inner-review-components">
+            Star Rating: {review.rating}
+          </div>
+          <div className="inner-review-components">
+            By: {review.User.userName}
+          </div>
+        </div>
       );
     });
   };
